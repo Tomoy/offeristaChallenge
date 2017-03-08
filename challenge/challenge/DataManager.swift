2eb1a17262bcfe208866940983aa8c073f4a270f
Tomás Ignacio Moyano <moyanotomasi@gmail.com>
2017-03-08 14:56:12 +0100

    basic organisation of groups, and class creation. Add cocoa pods to project and include Alamofire and AlamofireObjectMapper libraries

---
+++ challenge/challenge/DataManager.swift
@@ -0,0 +1,13 @@
+//
+//  DataManager.swift
+//  challenge
+//
+//  Created by Tomás Ignacio Moyano on 3/8/17.
+//  Copyright © 2017 Tomás Ignacio Moyano. All rights reserved.
+//
+
+import UIKit
+
+class DataManager: NSObject {
+
+}
